8bd3f41367dbc43200bfe53422d40172230859b2
hernandezurbina <hernandezurbina@gmail.com>
2020-03-02 10:42:48 +0000

    Created example with events

--- simple_kernel_launch.cu
+++ simple_kernel_launch.cu
@@ -8,12 +8,31 @@ __global__ void simple_kernel(){
 }
 
 int main(){
-  simple_kernel <<<1, 1>>>();
-  simple_kernel <<<1, 1>>>();
-  simple_kernel <<<1, 1>>>();
+
+  int dev = 0;
+  cudaDeviceProp deviceProp;
+  cudaGetDeviceProperties(&deviceProp, dev);
+
+  if(deviceProp.concurrentKernels == 0){
+    printf("GPU doesn't support concurrent execution\n");
+    printf("Kernel exec will be serialized\n");
+  }
+
+  cudaStream_t str1, str2, str3;
+  cudaStreamCreate(&str1);
+  cudaStreamCreate(&str2);
+  cudaStreamCreate(&str3);
+
+  simple_kernel <<<1, 1, 0, str1>>>();
+  simple_kernel <<<1, 1, 0, str2>>>();
+  simple_kernel <<<1, 1, 0, str3>>>();
+
+  cudaStreamDestroy(str1);
+  cudaStreamDestroy(str2);
+  cudaStreamDestroy(str3);
 
   cudaDeviceSynchronize();
   cudaDeviceReset();
-  
+
   return 0;
 }
